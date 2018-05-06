292bbc632f5a361a1ff0a15587c0e5a50bd6a52e
norberth_novanc <norberth.novanc@gmail.com>
2018-05-06 17:32:23 +0300

    refactored main class , removed descriptor.xml

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,15 +6,15 @@ import com.norberth.test.UserDetailsTO;
 import java.math.BigInteger;
 
 public class Main {
-
-    private static GenericConverterManager GENERIC_CONVERTER_MANAGER = null;
-
     public static void main(String[] args) {
         System.out.println("Started app...");
         GenericConverterManager gcm = GenericConverterManager.getInstance();
-        gcm.setDebug(true);
+        gcm.setDebug(false);
+//        for testing purposes we test entities in test package
         gcm.setPackageName("com.norberth.test");
         System.out.println("Initialized GenericConverterManager");
+
+//        Testing the manager
         UserDetails userDetails = new UserDetails("Novanc", new BigInteger("27"), "norberth.novanc@gmail.com", "password", "Norberth");
         UserDetailsTO userDetailsTO = (UserDetailsTO) gcm.getConverter(UserDetailsTO.class).getTo(userDetails);
         System.out.println(userDetails);
